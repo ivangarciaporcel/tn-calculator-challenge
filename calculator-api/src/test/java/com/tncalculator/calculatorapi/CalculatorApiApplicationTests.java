73e791b2398b67a2787b37c97597b15bffdbeaf3
Ivan Garcia <ivangp05@gmail.com>
2023-03-09 12:54:27 -0400

    Added initial skeleton of calculator api.

---
+++ calculator-api/src/test/java/com/tncalculator/calculatorapi/CalculatorApiApplicationTests.java
@@ -0,0 +1,1 @@
+package com.tncalculator.calculatorapi;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;@SpringBootTestclass CalculatorApiApplicationTests {    @Test    void contextLoads() {    }}
